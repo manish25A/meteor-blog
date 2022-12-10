edf8dbc374f972ec15b55720f1243db0783227f3
Manish B.C <manysh23@gmail.com>
2022-12-10 17:09:46 +0545

    change in package and remove unnecessary codes

--- imports/ui/components/Dashboard/Dashboard.vue
+++ imports/ui/components/Dashboard/Dashboard.vue
@@ -1,40 +1,6 @@
 <template>
-	<div id="App">
-		<BootstrapSidebar
-			:initial-show="initialShow"
-			:links="links"
-			:header="header"
-			:fa="true"
-			@sidebarChanged="onSidebarChanged"
-		>
-			<template v-slot:navbar>
-				<b-navbar
-					id="mainNavbar"
-					toggleable="lg"
-					type="light"
-					variant="light"
-					fixed="top"
-				>
-					<b-navbar-nav>
-						<b-nav-item> Navbar </b-nav-item>
-					</b-navbar-nav>
-				</b-navbar>
-			</template>
-
-			<template v-slot:content>
-				<b-container style="margin-top: 56px">
-					<router-view />
-				</b-container>
-			</template>
-		</BootstrapSidebar>
-	</div>
+	<h1>dashboard</h1>
 </template>
-
-<style lang="scss">
-/* @import "node_modules/bootstrap/scss/bootstrap";
-@import "node_modules/bootstrap-vue/src/index.scss"; */
-@import 'node_modules/vue-bootstrap-sidebar/src/scss/default-theme.scss';
-</style>
 
 <style>
 body {
