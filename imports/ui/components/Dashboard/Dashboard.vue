7fef7418439e73a3be59ee8d34a75fb2a0d57e3f
Manish B.C <manysh23@gmail.com>
2022-12-10 17:10:41 +0545

    remove unnecessary code

--- imports/ui/components/Dashboard/Dashboard.vue
+++ imports/ui/components/Dashboard/Dashboard.vue
@@ -9,44 +9,5 @@ body {
 </style>
 
 <script>
-import BootstrapSidebar from 'vue-bootstrap-sidebar';
-export default {
-	components: {
-		BootstrapSidebar,
-	},
-	data() {
-		return {
-			initialShow: true,
-			header: '<h3>Sidebar</h3>',
-			links: [
-				{ name: 'Home', href: { name: 'home' }, faIcon: ['fas', 'home'] },
-				{
-					name: 'Dropdown',
-					faIcon: ['fas', 'tint'],
-					children: [
-						{
-							name: 'Child Item 1',
-							href: {
-								name: 'child-item-1',
-							},
-							faIcon: ['fas', 'child'],
-						},
-						{
-							name: 'Child Item 2',
-							href: {
-								name: 'child-item-2',
-							},
-							faIcon: ['fas', 'child'],
-						},
-					],
-				},
-				{ name: 'About', href: { name: 'about' }, faIcon: 'users' },
-				{ name: 'Contact', href: { name: 'contact' }, faIcon: 'phone' },
-			],
-		};
-	},
-	methods: {
-		onSidebarChanged() {},
-	},
-};
+export default {};
 </script>
